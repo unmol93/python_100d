2dde627fcb3291135d18e4308c26887129c4c7cb
unmol93 <42358149+unmol93@users.noreply.github.com>
2024-01-15 13:41:39 +0530

    Update bid_check.py

--- day_9/bid_check.py
+++ day_9/bid_check.py
@@ -13,24 +13,38 @@ bid = int(input("bid price: "))
 dict_bid = {}
 
 def bid_start():
+ clear()
  print(logo)
- name = str(input("name: "))
- bid = int(input("bid price: "))
-  
+ 
+
 def func(nam,bidd):
  dict_bid[nam]= bidd
 
 func(name,bid)
-  
+
 more_bid = input("Any more bids?")
 
-if more_bid == "yes":
+while more_bid == "yes":
   bid_start()
+  name = input("Enter your name: ")
+  bid = int(input("Enter your bid: "))
   func(name,bid)
-else:
-  print("Go Home, Bid is over")
-  
-  
- 
+  print(f"Bids so far: {dict_bid}")
+  more_bid = input("Any more bids?")
+
+
+
+# Calculate and display the winner
+winner_name = max(dict_bid, key=dict_bid.get)
+winning_bid = dict_bid[winner_name]
+
+print(f"\nThe winner is {winner_name} with a bid of {winning_bid}.")
+
+
+
+
+
+
+
 
 
